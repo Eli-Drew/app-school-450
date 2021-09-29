67f06d44c9d3a9fce132dbdcbcc59bd7e17a6520
Eli-Drew <drew313@live.missouristate.edu>
2021-09-29 14:27:32 -0500

    US-01: Task 1.

--- us-02/us-02.py
+++ us-02/us-02.py
@@ -1,7 +1,10 @@
 """
+=================================================
 Author: Drew Rinker
-Test File for user story 2
+Date:   09/28/21
 
+This file takes care of us-02
+=================================================
 """
 import csv
 import os.path
@@ -45,6 +48,8 @@ def bomValidation(record):
         {'name': 'UTF-32 little-endian', 'sig': b'\xFF\xFE\x00\x00', 'encoding': 'utf-32-le'}]
 
     bomListOriginal = [b'\xEF\xBB\xBF', b'\xFE\xFF', b'\x00\x00\xFE\xFF', b'\xFF\xFE\x00\x00']
+
+    # this list will have to be added to so we can do this for any time of bom encoding
     bomList = ['ï»¿']
 
     # test = record[:3]
