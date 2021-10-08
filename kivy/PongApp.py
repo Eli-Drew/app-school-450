5438497c57590d81885075830e38615da72ae747
Eli-Drew <drew313@live.missouristate.edu>
2021-10-08 16:19:48 -0500

    fixed bomValidation in us-02. added us-02 documentation

--- kivy/PongApp.py
+++ kivy/PongApp.py
@@ -8,10 +8,14 @@ Author: Drew Rinker
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
 
 kivy.require('2.0.0')
 
 class PongGame(Widget):
+    pass
+
+class PongSlap(GridLayout):
     pass
 
 class PongApp(App):
