9dc83804c56836dfd84f1e1afc0b34229c44a1b2
hknehans <90802403+hknehans@users.noreply.github.com>
2021-10-08 21:14:49 -0500

    Change var from camelCase to snake_case to fit Python language and library convention and added block comments for functions

--- us-02/us-02.py
+++ us-02/us-02.py
@@ -20,55 +20,71 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from datetime import datetime
 import numpy as np
 
-#===================================================================================
-# Global variables
-#===================================================================================
-
-maxResponseLength = 500
+# I think we should stick to snake_case as that is the convention for Python and the libraries we are using uses snake_case
 
 #===================================================================================
-# semantic analysis functions
+# Global Variables
 #===================================================================================
+max_response_length = 500
 
-def tokenizeAndSequence(data):
-    # tokenizes an array of strings
-
+#===================================================================================
+# Semantic Analysis Functions
+#===================================================================================
+"""
+Tokenizes an array of strings into sequences and adds padding if necessary
+Paramaters:
+    data: the response(s) to be processed
+Returns:
+    void
+"""
+def pre_process(data):
     tokenizer = Tokenizer(num_words = 100000, oov_token="<OOV>")
     tokenizer.fit_on_texts(data)
     word_index = tokenizer.word_index
     # print(word_index)
 
     sequences = tokenizer.texts_to_sequences(data)
-    paddedSequences = pad_sequences(sequences, maxlen = 500)
+    padded_sequences = pad_sequences(sequences, maxlen = 500)
 
     print("Word Index: ", word_index)
     # print("Sequences: ", sequences)
-    print("Padded sequences: ", paddedSequences[0])
-    print(paddedSequences.shape)
+    print("Padded sequences: ", padded_sequences[0])
+    print(padded_sequences.shape)
 
 #===================================================================================
-# single reponse functions
+# Single Reponse Functions
 #===================================================================================
-
-def responseOption():
-    # this function will open a command line editor and limit the text area to 500 characters.
-    validResponse = False
-    while not validResponse:
-        responseValue = str(input("Enter in a response (max 500 characters): "))
-        if len(responseValue) > 500:
+"""
+This function will open a command line editor and limit the text area to 500 characters.
+Paramaters:
+    N/A
+Returns:
+    void
+"""
+def response_option():
+    valid_response = False
+    while not valid_response:
+        response_value = str(input("Enter in a response (max 500 characters): "))
+        if len(response_value) > 500:
             print("That was longer than 500 characters!")
         else:
-            validResponse = True
-    responseArray = []
-    responseArray.append(responseValue)
+            valid_response = True
+    response_array = []
+    response_array.append(response_value)
 
-    tokenizeAndSequence(responseArray)
+    pre_process(response_array)
 
 #===================================================================================
-# csv file functions
+# csv File Functions
 #===================================================================================
-
-def bomValidation(filename, default='utf-8'):
+"""
+This function will get rid of the 3 bom characters in the beginning if present
+Paramaters:
+    filename: the csv file of records to be validated
+Returns:
+    the record after validation
+"""
+def bom_validation(filename, default='utf-8'):
     msboms = dict((bom['sig'], bom) for bom in (
         {'name': 'UTF-8', 'sig': b'\xEF\xBB\xBF', 'encoding': 'utf-8'},
         {'name': 'UTF-16 big-endian', 'sig': b'\xFE\xFF', 'encoding':
@@ -87,89 +103,106 @@ def bomValidation(filename, default='utf-8'):
                 return msboms[sig[0:sl]]['encoding']
         return default
 
-def csvWrite(path):
-    # This makes a copy of the input csv with responses truncated to 500 chars
+"""
+This makes a copy of the input csv with responses truncated to 500 chars
+Paramaters:
+    path: the path of the csv file
+Returns:
+    N/A
+"""
+def csv_write(path):
     now = datetime.now()
-    nowFormat = now.strftime("%d/%m/%Y-%H.%M")
+    now_format = now.strftime("%d/%m/%Y-%H.%M")
 
-    writeFile = "new-" + path
+    write_file = "new-" + path
 
-def csvRead(path, encodingX):
-    # this function reads the csv file and makes sure it is formatted correctly. 
+"""
+This function reads the csv file and makes sure it is formatted correctly. 
+Paramaters:
+    path: the path of the csv file
+    encodingX:
+Returns:
+    data: response data from the csv file
+"""
+def csv_read(path, encodingX):
     # need to reorder some validation
 
     data = []
     
     count = 1
-    with open(path, newline='', encoding= encodingX) as csvFile:
-        csvReader = csv.reader(csvFile, delimiter='|')
-        for row in csvReader:
+    with open(path, newline='', encoding= encodingX) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='|')
+        for row in csv_reader:
 
-            currentRow = row[0]
+            current_row = row[0]
 
             # validate that there is only one element is each record.
             if len(row) != 1:
                 print("***Invalid .csv file***")
                 break
             else:
-                if len(row[0]) > maxResponseLength:
-                    print(currentRow + "\n" + "Length was: " + str(len(currentRow)))
-                    currentRow = currentRow[0:maxResponseLength] 
+                if len(row[0]) > max_response_length:
+                    print(current_row + "\n" + "Length was: " + str(len(current_row)))
+                    current_row = current_row[0:max_response_length] 
 
-                print(currentRow + "\n" + "Length: " + str(len(currentRow)))
+                print(current_row + "\n" + "Length: " + str(len(current_row)))
                 count += 1
 
-                data.append(currentRow)
+                data.append(current_row)
 
     return data
 
-
-def csvOption(): # dont change this to csv(). it will cause errors
-    # this function asks user for a path and then validates the path is valid. 
-    # this function is essentially the driver code for the .csv option.
-    validPath = False
-    while not validPath:
-        # TODO
-        # this will need to change. the user will have to enter full path or browse. 
-        # change entered path to the correct path on your computer
-        enteredFile = str(input("Enter name of the data set (with the extention): "))
-        enteredPath = "C:\\Users\\Drew\\Documents\\app-school-450\\data-sets\\" + enteredFile
-        print(enteredPath)
-        existingPath = path.exists(enteredPath)
-        if existingPath:
-            validPath = True
+"""
+This function is essentially the driver code for the .csv option
+This function asks user for a path and then validates the path is valid
+Don't change the name to csv(). or it will cause errors
+Paramaters:
+    N/A
+Returns:
+    N/A
+"""
+def csv_option(): 
+    valid_path = False
+    while not valid_path:
+        # TODO the user will have to enter full path or browse
+        entered_file = str(input("Enter name of the data set (with the extention): "))
+        entered_path = path.join(os.getcwd(), 'data-sets', entered_file) # this is dynamic and doesn't require us to change to our own path
+        print(entered_path)
+        path_exists = path.exists(entered_path)
+        if path_exists:
+            valid_path = True
             print("This is a valid path.")
         else:
             # continue just continues the loop. the else isn't nessecary but its good practice.
             print("That was not a valid path or file.")
-    endcoding = bomValidation(enteredPath)
-    newData = csvRead(enteredPath, endcoding)
+    endcoding = bom_validation(entered_path)
+    new_data = csv_read(entered_path, endcoding)
 
     # tokenize
-    tokenizeAndSequence(newData)
+    pre_process(new_data)
     
 #===================================================================================
-# driver code
+# Driver Code
 #===================================================================================
 
 def main():
-    validEntry = False
-    validEntries = ['r','R','c','C']
+    valid_entry = False
+    valid_entries = ['r','R','c','C']
 
-    while (not validEntry):
+    while (not valid_entry):
 
         print("What would you like to like to do? \nImport a .csv file? Or enter in a response? \n ")
         option = str(input("Press 'c' for .csv. Press 'r' for response: "))
         
-        if option in validEntries:
-            validEntry = True
-
+        if option in valid_entries:
+            valid_entry = True
+        
         else:
             print("\n" + "=" * 50)
             print("Enter a vaild response.")
             print("=" * 50)
 
-    responseOption() if (option == 'r' or option == 'R') else csvOption()
+    response_option() if (option == 'r' or option == 'R') else csv_option()
     
 
 if __name__ == "__main__":
