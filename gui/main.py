a4fb6b25887fbabf271a92aff17b0f314a64b27b
Brent <bla1997@live.missouristate.edu>
2021-11-20 15:09:37 -0600

    aaaaaaaaa

--- gui/main.py
+++ gui/main.py
@@ -20,21 +20,14 @@ from subprocess import Popen
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.lang import Builder
 import os
 import nltk
 import AnalysisReportApp
-import config
 nltk.download('stopwords')
 kivy.require('2.0.0')
 
 
-class FratForLife(Screen):
+class FratForLife(Widget):
     #csv_txt_input = ObjectProperty(None)
 
     def main(self):
@@ -45,14 +38,38 @@ class FratForLife(Screen):
 
         # C:/Users/Brentlee/Downloads/demoData.csv <- Example file location input
         # get user input and clean the file data
+        data = FratForLife.getData(self.ids.csv_txt_input.text)
+        cleanData = FratForLife.getCleanData(data)
 
-        pass
+        # tokenize and vectorize the data
+        # TFIDF = (occurences of word in document / total number of words in document) * (log[total # of documents in corpus / number of documents containing word])
+        vectorizer = TfidfVectorizer(max_features=1000)
+        vectorData = vectorizer.fit_transform(cleanData)
 
-        # AnalysisReportApp.AnalysisReportApp.topics(topics)
+        # Non-Negative Matrix Factorization model...
+        '''
+        Given a matrix M x N, where M = Total number of documents and N = total number of words,
+        NMF is the matrix decompostition that generates the Features with M rows and K columns,
+        where K = total number of topics and the Components matrix is the matrix of K by N.
+        The Product of the Features and Components matricies results in the approximation of the TF-IDF.
+        '''
+        thematic_model = load('thematic_model')
+        thematic_model.fit_transform(vectorData)
+
+        # nmf_model = NMF(n_components=5, init='random', random_state=0)
+        # nmf_model.fit_transform(vectorData)
+
+        # get the feature names and print the topics from the model
+        featureNames = vectorizer.get_feature_names()
+        topics = FratForLife.getTopics(
+            thematic_model.components_, featureNames)
+
+        AnalysisReportApp.AnalysisReportApp.topics(topics)
 
         # topic_one = str(topics[0])
         # self.ids.topic_text.text = topic_one
-        # open_close(self)
+        Popen(['python', 'AnalysisReportApp.py'])
+        FratApp().stop()
 
     def toggle_disable_inputs(self):
         if(self.ids.csv_txt_input.disabled is True):
@@ -87,168 +104,78 @@ class FratForLife(Screen):
         #true_file_name = os.path.join(path, filename[0])
         self.dismiss_popup()
 
+    '''
+    def __init__(self, **kwargs):
+        super(FratForLife, self).__init__(**kwargs)
+        with self.canvas.before:
+            Color(0, 1, 0)
+    '''
 
-class AnalysisReportApp(Screen):
-    def build(self):
+    """
+    ===================================================================
+    Description: getData returns the data located at the fileName param
+    Param(s): fileName path for data.
+    Returns: dataset as list of one element (entire dataset as one elem)
+    ===================================================================
+    """
 
-        #topics = main.topic_list
+    def getData(fileName):
+        # open file
+        data = open(fileName, "r", encoding='UTF8')
+        # split the file data
+        splitData = data.read().replace('"', ' ').replace('’', '\'').split('\n\n')
+        # convert all text to lowercase and replace all newlines with a space
+        realData = [x.lower().replace('\n', ' ')
+                    for x in splitData if len(x) > 500]
+        return realData
 
-        Window.clearcolor = (0, 14/255, 52/255, 1)
+    """
+    ===============================================================
+    Description: getCleanData returns its data param after cleaning
+    Param(s): data list (from getData())
+    Returns: A list of text, for example:
 
-        layout = BoxLayout(orientation='vertical')
+    ['great', 'great presentation', 'great presentation clear',...]
 
-        title = Label(text='Feedack Response Analysis Report',
-                      size_hint_y=None, height=64, padding=(8, 8))
+    ===============================================================
+    """
 
-        content = BoxLayout(orientation='horizontal')
+    def getCleanData(data):
+        cleanData = []
+        # stop words are words that lack context meaning but occur frequently in natural language
+        stop_words = set(stopwords.words('english'))
+        for d in data:
+            tokens = ' '.join(TextBlob(d).noun_phrases).split()
+            cleanD = []
+            for w in tokens:
+                # convert words to their base form, like plural to singular
+                w = Word(w).lemmatize()
+                if w not in stop_words and len(w) > 4:
+                    cleanD.append(w)
+                if len(cleanD) != 0:
+                    cleanData.append(' '.join(cleanD))
+        return cleanData
 
-        sidebar = BoxLayout(orientation='vertical',
-                            size_hint_x=None, width=200)
+    """
+    =================================================================================
+    Description: getTopics prints the the repective topics and their components
+    Param(s): model components, feature names, and n = the number of words per topic.
+    Returns: Nothing, just prints topics (at least for now).
+    =================================================================================
+    """
 
-        tpc1_btn = Button(text='topic-1')
-        # =============================================
-        # TODO get topics into the widgets on this side
-        # =============================================
-        #tpc1_btn.text = topic_1
-        sidebar.add_widget(tpc1_btn)
-        sidebar.add_widget(Button(text='topic-2'))
-        sidebar.add_widget(Button(text='topic-3'))
-        sidebar.add_widget(Button(text='topic-4'))
-        sidebar.add_widget(Button(text='topic-5'))
-        sidebar.add_widget(Button(text='export'))
-        again_btn = Button(text='again')
-        again_btn.bind(on_release=AnalysisReportApp.close_open)
-        sidebar.add_widget(again_btn)
-
-        grid = GridLayout(cols=2)
-
-        summary = BoxLayout(orientation='vertical')
-        summary.add_widget(Label(text='summary'))
-        summary.add_widget(Label(text='content', size_hint_y=None, height=32))
-
-        chart = BoxLayout(orientation='vertical')
-        chart.add_widget(Label(text='chart'))
-        chart.add_widget(Label(text='content', size_hint_y=None, height=32))
-
-        topic1 = config.topic_list[0]
-
-        topics1 = BoxLayout(orientation='vertical')
-        topics1.add_widget(Label(text='topics1' + topic1))
-        topics1.add_widget(Label(text='content', size_hint_y=None, height=32))
-
-        topics2 = BoxLayout(orientation='vertical')
-        topics2.add_widget(Label(text='topics2'))
-        topics2.add_widget(Label(text='content', size_hint_y=None, height=32))
-
-        grid.add_widget(summary)
-        grid.add_widget(chart)
-        grid.add_widget(topics1)
-        grid.add_widget(topics2)
-
-        layout.add_widget(title)
-
-        content.add_widget(sidebar)
-        content.add_widget(grid)
-
-        layout.add_widget(content)
-
-        return layout
-
-
-class WindowManager(ScreenManager):
-    pass
-
-
-'''
-def __init__(self, **kwargs):
-    super(FratForLife, self).__init__(**kwargs)
-    with self.canvas.before:
-        Color(0, 1, 0)
-'''
-
-"""
-===================================================================
-Description: getData returns the data located at the fileName param
-Param(s): fileName path for data.
-Returns: dataset as list of one element (entire dataset as one elem)
-===================================================================
-"""
-
-
-def getData(fileName):
-    # open file
-    data = open(fileName, "r", encoding='UTF8')
-    # split the file data
-    splitData = data.read().replace('"', ' ').replace('’', '\'').split('\n\n')
-    # convert all text to lowercase and replace all newlines with a space
-    realData = [x.lower().replace('\n', ' ')
-                for x in splitData if len(x) > 500]
-    return realData
-
-
-"""
-===============================================================
-Description: getCleanData returns its data param after cleaning
-Param(s): data list (from getData())
-Returns: A list of text, for example:
-
-['great', 'great presentation', 'great presentation clear',...]
-
-===============================================================
-"""
-
-
-def getCleanData(data):
-    cleanData = []
-    # stop words are words that lack context meaning but occur frequently in natural language
-    stop_words = set(stopwords.words('english'))
-    for d in data:
-        tokens = ' '.join(TextBlob(d).noun_phrases).split()
-        cleanD = []
-        for w in tokens:
-            # convert words to their base form, like plural to singular
-            w = Word(w).lemmatize()
-            if w not in stop_words and len(w) > 4:
-                cleanD.append(w)
-            if len(cleanD) != 0:
-                cleanData.append(' '.join(cleanD))
-    return cleanData
-
-
-"""
-=================================================================================
-Description: getTopics prints the the repective topics and their components
-Param(s): model components, feature names, and n = the number of words per topic.
-Returns: Nothing, just prints topics (at least for now).
-=================================================================================
-"""
-
-
-def getTopics(components, feature_names, n=50):
-    config.init()
-    for idx, topic in enumerate(components):
-        config.topic_list.append([(feature_names[i], topic[i].round(2))
-                                  for i in topic.argsort()[:-n - 1:-1]])
-
-
-def open_close(self):
-    Popen(['python', 'AnalysisReportApp.py'])
-    FratApp().stop()
-
-
-def close_open(self):
-    AnalysisReportApp().stop()
-    Popen(['python', 'main.py'])
+    def getTopics(components, feature_names, n=50):
+        topic_list = []
+        for idx, topic in enumerate(components):
+            topic_list.append([(feature_names[i], topic[i].round(2))
+                               for i in topic.argsort()[:-n - 1:-1]])
+        return topic_list
 
 
 class FratApp(App):
     def build(self):
-        global root
-        global csv_txt_input
-        root = FratForLife()
-        csv_txt_input = root.ids.csv_txt_input
         Window.size = (1920, 1080)
-        return Builder.load_file("Frat.kv")
+        return FratForLife()
 
 
 class LoadDialog(FloatLayout):
